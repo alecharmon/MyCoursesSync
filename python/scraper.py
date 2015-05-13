0eacc1fe12d75e3dc0fe9a4c8d53004884cc529f
Shardul Baral <shardul@wpa063039.wireless.mcgill.ca>
2015-05-13 11:49:34 -0400

    fix it pls alec

--- python/scraper.py
+++ python/scraper.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-#driver = webdriver.PhantomJS()
-driver = webdriver.Firefox()
-driver.get("https://mymcgill.mcgill.ca/portal/page/portal/Login")
-driver.find_element_by_id('username').send_keys(pw)
-driver.find_element_by_id('password').send_keys("",Keys.RETURN)
-driver.find_element_by_link_text('Access myCourses').click();
-print driver.current_url
+
+def login(user, passw):
+	driver = webdriver.Firefox()
+	driver.get("https://mymcgill.mcgill.ca/portal/page/portal/Login")
+	driver.find_element_by_id('username').send_keys(user)
+	driver.find_element_by_id('password').send_keys(passw, Keys.RETURN)
+	driver.find_element_by_link_text('Access myCourses').click();
+	return driver.current_url
+
+print(login("shardul.baral@mail.mcgill.ca", "beauties"))
+
